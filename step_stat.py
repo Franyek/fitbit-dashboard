c1be2643e02cc3952f9e8919583c1b2e3331f6c8
Franciska Sara <f.sara@snapaddy.com>
2017-10-28 22:18:47 +0200

    feature(workday steps diagram)

--- step_stat.py
+++ step_stat.py
@@ -1,10 +1,12 @@
 import configparser
+import datetime
 import os
 
 import fitbit
 from plotly.graph_objs import Scatter
 from plotly.offline import plot
 
+from running_object import get_runs
 
 config = configparser.ConfigParser()
 folder_name = os.path.dirname(os.path.realpath(__file__))
@@ -18,21 +20,24 @@ authd_client = fitbit.Fitbit(CLIENT, SECRET,
                              access_token=ACCESS_TOKEN,
                              refresh_token=REFRESH_TOKEN)
 
-response = authd_client.make_request(
-    'https://api.fitbit.com/1/user/-/activities/list.json?beforeDate=2017-06-01&offset=0&limit=20&sort=asc')
-sleep = authd_client.body_weight_goal()
-alma = authd_client.time_series('activities/steps', period='1y', base_date='2017-05-01')
-act_list = authd_client.activities_list()
-running = authd_client.activity_detail('12030')
+activities_steps = authd_client.time_series('activities/steps', period='3m')
+#activities_runs = get_runs(authd_client)
+
+holiday_first_date = datetime.date(2017, 8, 21)
+invalid_dates = [holiday_first_date + datetime.timedelta(days=x) for x in range(0, 7)]
+invalid_dates.append(datetime.date(2017, 9, 21))
+invalid_dates.append(datetime.date(2017, 9, 22))
+invalid_dates.append(datetime.date(2017, 9, 25))
 di = {}
-for a in alma['activities-steps']:
+for a in activities_steps['activities-steps']:
     dm = a['dateTime']
-    # print("{0}\t{1}".format(dm, a['value']))
-    dm = dm[0:7]
-    if dm in di:
-        di[dm].append(a['value'])
-    else:
-        di[dm] = [a['value']]
+    date_parts = dm.split('-')
+    today = datetime.date(int(date_parts[0]), int(date_parts[1]), int(date_parts[2]))
+    if (today.weekday() < 5) and today not in invalid_dates and int(a['value']) != 0:
+        if dm in di:
+            di[dm].append(a['value'])
+        else:
+            di[dm] = [a['value']]
 ordered_dates = di.keys()
 sorted(ordered_dates)
 sepfile = []
@@ -50,9 +55,20 @@ y = [value for (date, value) in sepfile]
 # Create a trace
 trace = Scatter(
     x=x,
-    y=y
+    y=y,
+    mode='lines+markers',
+    name='Daily steps'
 )
 
-data = [trace]
+reference_line = Scatter(
+    x=x,
+    y=[8500] * len(x),
+    name='Reference line',
+    line=dict(
+        color=('rgb(205, 12, 24)'),
+        width=5)
+)
 
-plot(data, filename='basic-line')
+data = [trace, reference_line]
+
+plot(data, filename='basic-area')
